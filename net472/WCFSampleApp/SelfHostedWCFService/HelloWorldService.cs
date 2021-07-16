bb05beca91ed7897400fb74b70eed41c51e23787
Kanishk Kaul <kanikaul@amazon.com>
2021-07-16 13:18:39 -0700

    Add Self Hosted WCFProject

---
+++ net472/WCFSampleApp/SelfHostedWCFService/HelloWorldService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfHostedWCFService
+{
+    [ServiceContract]
+    public interface IHelloWorldService
+    {
+        [OperationContract]
+        string SayHello(string name);
+    }
+    public class HelloWorldService : IHelloWorldService
+    {
+        public string SayHello(string name)
+        {
+            return String.Format("Hello, {0}", name);
+        }
+    }
+}
