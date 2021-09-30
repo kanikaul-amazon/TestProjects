73a223baaa65a26594530a31ca5d90c60a1efe37
Kanishk Kaul <kanikaul@amazon.com>
2021-09-29 18:55:59 -0700

    WCF Test Samples

---
+++ net472/WCFTestSamples/Client/IEchoService.cs
@@ -0,0 +1,40 @@
+﻿using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace DotnetCoreWCFClient
+{
+
+    [ServiceContract]
+    public interface IEchoService
+    {
+
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: Add your service operations here
+    }
+
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
